8ffe762530418840cc0617ffe8b06d4229354f1e
Rudik Manukyan <rudik770@gmail.com>
2025-09-16 11:58:54 +0400

    7.Попытка номер раз

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,45 +1,52 @@
 export function initFiltering(elements) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    const updateIndexes = (elements, indexes) => {Object.keys(indexes).forEach((elementName) => {
-        elements[elementName].append(
-            ...Object.values(indexes[elementName]).map(name => {
-                const el = document.createElement('option');
-                el.value = name;
-                el.textContent = name;
-                return el;
-            })
-        );
+  // @todo: #4.1 — заполнить выпадающие списки опциями
+  const updateIndexes = (elements, indexes) => {
+    Object.keys(indexes).forEach((elementName) => {
+      elements[elementName].append(
+        ...Object.values(indexes[elementName]).map((name) => {
+          const el = document.createElement("option");
+          el.value = name;
+          el.textContent = name;
+          return el;
+        })
+      );
     });
+  };
+
+  const applyFiltering = (query, state, action) => {
+    // @todo: #4.2 — обработать очистку поля
+    if (action && action.name === "clear") {
+      const parent = action.parentElement;
+      const input = parent.querySelector("select, input");
+      const field = action.dataset.field;
+
+      if (input) input.value = "";
+      if (field && field in state) {
+        delete state[field];
+      }
     }
 
-    const applyFiltering = (query, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
-        if (action && action.name === 'clear') {
-            const parent = action.parentElement;
-            const input = parent.querySelector('select, input');
-            const field = action.dataset.field;
-
-            if (input) input.value = '';
-            if (field && field in state) {
-                delete state[field];
-            }
+    // @todo: #4.5 — отфильтровать данные используя компаратор
+    const filter = {};
+    Object.keys(elements).forEach((key) => {
+      if (elements[key]) {
+        if (
+          ["INPUT", "SELECT"].includes(elements[key].tagName) &&
+          elements[key].value
+        ) {
+          // ищем поля ввода в фильтре с непустыми данными
+          filter[`filter[${elements[key].name}]`] = elements[key].value; // чтобы сформировать в query вложенный объект фильтра
         }
+      }
+    });
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-     const filter = {};
-     Object.keys(elements).forEach(key => {
-         if (elements[key]) {
-              if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) { // ищем поля ввода в фильтре с непустыми данными
-                    filter[`filter[${elements[key].name}]`] = elements[key].value; // чтобы сформировать в query вложенный объект фильтра
-             }
-        }
-     })
+    return Object.keys(filter).length
+      ? Object.assign({}, query, filter)
+      : query; // если в фильтре что-то добавилось, применим к запросу
+  };
 
-    return Object.keys(filter).length ? Object.assign({}, query, filter) : query; // если в фильтре что-то добавилось, применим к запросу
-    }
-
-    return {
-        updateIndexes,
-        applyFiltering
-    }
+  return {
+    updateIndexes,
+    applyFiltering,
+  };
 }
