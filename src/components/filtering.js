ee625eacf6c2187bf3a773df23d9870fe15e07a9
Rudik Manukyan <rudik770@gmail.com>
2025-09-16 11:53:51 +0400

    7.Попытка номер раз

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,22 +1,18 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
-
-// @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules); 
-
-export function initFiltering(elements, indexes) {
+export function initFiltering(elements) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes).forEach((elementName) => {
+    const updateIndexes = (elements, indexes) => {Object.keys(indexes).forEach((elementName) => {
         elements[elementName].append(
             ...Object.values(indexes[elementName]).map(name => {
-                const option = document.createElement('option');
-                option.value = name;
-                option.textContent = name;
-                return option;
+                const el = document.createElement('option');
+                el.value = name;
+                el.textContent = name;
+                return el;
             })
         );
     });
+    }
 
-    return (data, state, action) => {
+    const applyFiltering = (query, state, action) => {
         // @todo: #4.2 — обработать очистку поля
         if (action && action.name === 'clear') {
             const parent = action.parentElement;
@@ -30,6 +26,20 @@ export function initFiltering(elements, indexes) {
         }
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state)); 
+     const filter = {};
+     Object.keys(elements).forEach(key => {
+         if (elements[key]) {
+              if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) { // ищем поля ввода в фильтре с непустыми данными
+                    filter[`filter[${elements[key].name}]`] = elements[key].value; // чтобы сформировать в query вложенный объект фильтра
+             }
+        }
+     })
+
+    return Object.keys(filter).length ? Object.assign({}, query, filter) : query; // если в фильтре что-то добавилось, применим к запросу
+    }
+
+    return {
+        updateIndexes,
+        applyFiltering
     }
 }
